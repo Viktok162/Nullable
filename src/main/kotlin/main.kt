f4fef26f268778eec24c0dcbecafe8daea3f04ba
Viktok <viktok162@gmail.com>
2025-04-01 21:13:00 +0500

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,92 @@
+import WallService.add
+import WallService.postsAny
+import WallService.sizeOfPosts
+import WallService.update
+
+
+fun main() {
+    val postIni = Post(0, likes = Post.Like(0, false, false, false))
+
+    add(postIni)
+    add(postIni)
+    add(postIni)
+    add(postIni)
+    add(postIni)
+
+    println("posts size is = ${sizeOfPosts()}")
+    postsAny(1)
+
+    val sample = Post(3, copyright = "Super", likes = Post.Like(500, canLike = true))
+    println(update(sample))
+}
+
+data class Post(
+    var id: Int? = 0,
+    val ownerId: Int = 0,
+    val formId: Int? = 0,
+    val createdBy: Int = 0,
+    val text: String? = "Hello!",
+    val copyright: String = "protected",
+    val postType: String = "brief story",
+    val canPin: Boolean? = false,
+    val canDelete: Boolean = false,
+    val canEdit: Boolean = false,
+    val likes: Like?
+) {
+    init {
+        println("\nInit: New instance created, id = $id")
+    }
+
+    fun showDataPost() {
+        println(
+            "Данные объекта post: id = $id, ownerId = $ownerId, formId = $formId," +
+                    " copyright = $copyright, canPin = $canPin, $text, likes = ${likes?.count}, canLike = ${likes?.canLike}"
+        )
+    }
+
+    class Like(
+        val count: Int = 0,
+        val userLikes: Boolean = false,
+        val canLike: Boolean = false,
+        val canPublish: Boolean = false
+    )
+}
+
+object WallService {
+    var posts = mutableListOf<Post>()
+    private var number: Int = 0
+
+    fun add(post: Post): Post {
+        number++
+        val p = post.copy(id = number)
+        posts += p
+        return posts.last()
+    }
+
+    fun update(post: Post): Boolean {
+        for (i in posts.indices) {
+            if (post.id == posts[i].id) {
+                posts[i] = post
+                //posts[i].showDataPost()
+                return true
+            }
+        }
+        return false
+    }
+
+    fun sizeOfPosts(): Int {
+        return posts.size
+    }
+
+    fun postsAny(index: Int) {
+        println("Element $index of posts:")
+        posts[index].showDataPost()
+    }
+
+    fun resetNumber() {
+        number = 0
+    }
+    fun resetPosts() {
+        posts.clear()
+    }
+}
---
+++ src/test/kotlin/WallServiceTest.kt
@@ -0,0 +1,43 @@
+import org.junit.Test
+import org.junit.Assert.*
+import org.junit.Before
+
+class WallServiceTest {
+
+    @Before
+    fun clearBeforeTest() {
+        WallService.resetNumber()
+        WallService.resetPosts()
+    }
+
+    @Test
+    fun add_Test_True() {
+        val postTest1 = Post(0, likes = Post.Like())
+        val postAdded = WallService.add(postTest1)
+        var result = false
+        if (postAdded.id != 0) result = true
+        assertTrue(result)
+    }
+
+    @Test
+    fun update_True() {
+        val postTest2 = Post(0, likes = Post.Like())
+        WallService.add(postTest2)
+        WallService.add(postTest2)
+        WallService.add(postTest2)
+        val sampleTest = Post(2, copyright = "Super", likes = Post.Like(500, canLike = true))
+        val result: Boolean = WallService.update(sampleTest)
+        assertTrue(result)
+    }
+
+    @Test
+    fun update_False() {
+        val postTest3 = Post(0, likes = Post.Like())
+        WallService.add(postTest3)
+        WallService.add(postTest3)
+        WallService.add(postTest3)
+        val sample = Post(4, copyright = "Super", likes = Post.Like(500, canLike = true))
+        val result: Boolean = WallService.update(sample)
+        assertFalse(result)
+    }
+}
