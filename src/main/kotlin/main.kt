0a3bb31adda73424b9a4058d8a8a88f3296de15a
Viktok <viktok162@gmail.com>
2025-04-05 23:57:13 +0500

    Attachments

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,7 +5,12 @@ import WallService.update
 
 
 fun main() {
-    val postIni = Post(0, likes = Post.Like(0, false, false, false))
+    val photo = Attachment.Photo(id = 22, ownerId = 22, photo130 = "link1", photo604 = "link2")
+    val video = Attachment.Video(id = 33, ownerId = 33, duration = 10)
+    val audio = Attachment.Audio(id = 44, ownerId = 44, title = "Audio", url = "link3")
+
+    val postIni = Post(0, likes = Post.Likes(10), attachments = listOf(photo, video, audio))
+    postIni.showDataPost()
 
     add(postIni)
     add(postIni)
@@ -16,8 +21,34 @@ fun main() {
     println("posts size is = ${sizeOfPosts()}")
     postsAny(1)
 
-    val sample = Post(3, copyright = "Super", likes = Post.Like(500, canLike = true))
+    val sample = Post(3, copyright = "Super", likes = Post.Likes(500, canLike = true))
     println(update(sample))
+}
+
+abstract class Attachment(val type: String) {
+
+    data class Photo(
+        val id: Int = 1,
+        val ownerId: Int = 1,
+        val photo130: String = "some link to the object",
+        val photo604: String = "another link to the object"
+    ) : Attachment("photo")
+
+    data class Video(
+        val id: Int = 1,
+        val ownerId: Int = 1,
+        val title: String = "A Funny Video",
+        val duration: Int = 30
+    ) : Attachment("video")
+
+    data class Audio(
+        val id: Int = 1,
+        val ownerId: Int = 1,
+        val artist: String = "Names of artists",
+        val title: String = "Title of audio",
+        val duration: Int = 5,
+        val url: String = "link to the object"
+    ) : Attachment("audio")
 }
 
 data class Post(
@@ -31,7 +62,8 @@ data class Post(
     val canPin: Boolean? = false,
     val canDelete: Boolean = false,
     val canEdit: Boolean = false,
-    val likes: Like?
+    val likes: Likes = Likes(),
+    val attachments: List<Attachment> = emptyList()
 ) {
     init {
         println("\nInit: New instance created, id = $id")
@@ -40,11 +72,11 @@ data class Post(
     fun showDataPost() {
         println(
             "Данные объекта post: id = $id, ownerId = $ownerId, formId = $formId," +
-                    " copyright = $copyright, canPin = $canPin, $text, likes = ${likes?.count}, canLike = ${likes?.canLike}"
+                    " copyright = $copyright, canPin = $canPin, $text, likes = ${likes.count}, canLike = ${likes.canLike}"
         )
     }
 
-    class Like(
+    class Likes(
         val count: Int = 0,
         val userLikes: Boolean = false,
         val canLike: Boolean = false,
@@ -57,8 +89,7 @@ object WallService {
     private var number: Int = 0
 
     fun add(post: Post): Post {
-        number++
-        val p = post.copy(id = number)
+        val p = post.copy(id = ++number)
         posts += p
         return posts.last()
     }
@@ -86,6 +117,7 @@ object WallService {
     fun resetNumber() {
         number = 0
     }
+
     fun resetPosts() {
         posts.clear()
     }
--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -12,7 +12,7 @@ class WallServiceTest {
 
     @Test
     fun add_Test_True() {
-        val postTest1 = Post(0, likes = Post.Like())
+        val postTest1 = Post(0, likes = Post.Likes())
         val postAdded = WallService.add(postTest1)
         var result = false
         if (postAdded.id != 0) result = true
@@ -21,22 +21,22 @@ class WallServiceTest {
 
     @Test
     fun update_True() {
-        val postTest2 = Post(0, likes = Post.Like())
+        val postTest2 = Post(0, likes = Post.Likes())
         WallService.add(postTest2)
         WallService.add(postTest2)
         WallService.add(postTest2)
-        val sampleTest = Post(2, copyright = "Super", likes = Post.Like(500, canLike = true))
+        val sampleTest = Post(2, copyright = "Super", likes = Post.Likes(500, canLike = true))
         val result: Boolean = WallService.update(sampleTest)
         assertTrue(result)
     }
 
     @Test
     fun update_False() {
-        val postTest3 = Post(0, likes = Post.Like())
+        val postTest3 = Post(0, likes = Post.Likes())
         WallService.add(postTest3)
         WallService.add(postTest3)
         WallService.add(postTest3)
-        val sample = Post(4, copyright = "Super", likes = Post.Like(500, canLike = true))
+        val sample = Post(4, copyright = "Super", likes = Post.Likes(500, canLike = true))
         val result: Boolean = WallService.update(sample)
         assertFalse(result)
     }
